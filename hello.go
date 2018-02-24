8952ea623ac4e19038092733488a028925acff49
jithhuawei <34733024+jithhuawei@users.noreply.github.com>
2018-02-24 16:42:09 +0530

    Add files via upload

---
+++ hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() int {
+    fmt.Println("Hello World. !!!")
+
+    return 1
+}
---
+++ sumGolang.go
@@ -0,0 +1,16 @@
+package sumGolang
+//import "fmt"
+
+func sumGolang() int {
+
+  var first int
+  var second int
+  
+  first = 10
+  second = 20
+
+  var sum = first + second
+  //fmt.Println ("Hello Jithu. !!!")
+  return sum
+
+}
